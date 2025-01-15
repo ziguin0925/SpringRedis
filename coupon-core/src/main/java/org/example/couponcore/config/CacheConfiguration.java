cd45d252c0713c9bd7c07560e363693824f6b61b
RG <jlwoo092513@gmail.com>
2025-01-15 23:48:26 +0900

    :sparkles: [Feat] : RedisCacheConfiguration 등록
    1. 강의상에서는 리턴타입이 CacheManager라고 적혀있지만 RedisCacheManager로 리턴타입을 바꿔줘야 반환이됨.
    -> 로직 수행되는지 확인 필요.

---
+++ coupon-core/src/main/java/org/example/couponcore/config/CacheConfiguration.java
@@ -0,0 +1,31 @@
+package org.example.couponcore.config;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@RequiredArgsConstructor
+public class CacheConfiguration {
+
+    private final RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public RedisCacheManager redisCacheManager() {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(30));
+
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
